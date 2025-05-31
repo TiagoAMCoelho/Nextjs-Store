4bf0bb3673e23f13f10e852a8ad79bb85ed5e080
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-05-31 19:53:21 +0100

    DATABASE_URL changed due to an error on config and prisma create method practice

--- Nextjs-Store/app/about/page.tsx
+++ Nextjs-Store/app/about/page.tsx
@@ -1,4 +1,21 @@
-function AboutPage() {
-  return <div>AboutPage</div>;
+import db from "@/utils/db";
+
+async function AboutPage() {
+  const profile = await db.testProfile.create({
+    data: {
+      name: "random name",
+      age: 20,
+    },
+  });
+
+  const users = await db.testProfile.findMany();
+
+  return (
+    <div>
+      {users.map((user) => {
+        return <h2 key={user.id}>{user.name}</h2>;
+      })}
+    </div>
+  );
 }
 export default AboutPage;
