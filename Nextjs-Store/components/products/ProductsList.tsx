66251664734bacd9961e7a349e8f1e03b8f7855d
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-05-31 23:34:49 +0100

    Enhance AboutPage with a styled header and description; update HomePage to use Suspense for FeaturedProducts; implement ProductsPage with ProductsContainer; improve LoadingContainer with skeleton loading; expand ProductsContainer to include layout options and product display; define ProductsList to accept product props; add loading component for products.

--- Nextjs-Store/components/products/ProductsList.tsx
+++ Nextjs-Store/components/products/ProductsList.tsx
@@ -1,4 +1,10 @@
-function ProductsList() {
+import { Product } from "@prisma/client";
+
+interface Props {
+  products: Product[];
+}
+
+function ProductsList({ products }: Props) {
   return <div>ProductsList</div>;
 }
 export default ProductsList;
