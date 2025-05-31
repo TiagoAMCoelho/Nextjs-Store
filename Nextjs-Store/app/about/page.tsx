20f89408c3ff5b8dd853fd882ed866c5bf6e0242
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-05-31 19:58:08 +0100

    Product Model

--- Nextjs-Store/app/about/page.tsx
+++ Nextjs-Store/app/about/page.tsx
@@ -1,21 +1,6 @@
 import db from "@/utils/db";
 
-async function AboutPage() {
-  const profile = await db.testProfile.create({
-    data: {
-      name: "random name",
-      age: 20,
-    },
-  });
-
-  const users = await db.testProfile.findMany();
-
-  return (
-    <div>
-      {users.map((user) => {
-        return <h2 key={user.id}>{user.name}</h2>;
-      })}
-    </div>
-  );
+function AboutPage() {
+  return <div>AboutPage</div>;
 }
 export default AboutPage;
