6955c0c35a7f48dc61c4714da14b4054a9b1b828
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-06-02 22:49:24 +0100

    Add authentication and user management features with Clerk integration; update layout and navbar components to support user sign-in and sign-out functionality.

--- Nextjs-Store/app/providers.tsx
+++ Nextjs-Store/app/providers.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 import { ReactNode } from "react";
 
 function Providers({ children }: { children: ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
