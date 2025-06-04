66045efeb99186a886d4e9b2269d8a127aca565a
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-06-04 23:25:37 +0100

    Add user authentication to createProductAction; implement error handling and user validation

--- Nextjs-Store/utils/actions.ts
+++ Nextjs-Store/utils/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { log } from "console";
 import { redirect } from "next/navigation";
 
 export const fetchFeaturedProducts = async () => {
@@ -10,6 +12,19 @@ export const fetchFeaturedProducts = async () => {
     },
   });
   return products;
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
 };
 
 export const fetchAllProducts = ({ search = "" }: { search: string }) => {
@@ -40,5 +55,31 @@ export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return { message: "product created" };
+  const user = await getAuthUser();
+
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    //temp
+    const image = formData.get("image") as string;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/product-1.jpg",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
