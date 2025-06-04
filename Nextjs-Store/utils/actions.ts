0deda890f40be1b71d13a3f2a9d0eb9b3b358be0
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-06-04 23:32:29 +0100

    Add product validation schema and refactor createProductAction to use schema for input validation

--- Nextjs-Store/utils/actions.ts
+++ Nextjs-Store/utils/actions.ts
@@ -4,6 +4,7 @@ import db from "@/utils/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { log } from "console";
 import { redirect } from "next/navigation";
+import { productSchema } from "./schemas";
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -58,25 +59,9 @@ export const createProductAction = async (
   const user = await getAuthUser();
 
   try {
-    const name = formData.get("name") as string;
-    const company = formData.get("company") as string;
-    const price = Number(formData.get("price") as string);
-    //temp
-    const image = formData.get("image") as string;
-    const description = formData.get("description") as string;
-    const featured = Boolean(formData.get("featured") as string);
+    const rawData = Object.fromEntries(formData);
 
-    await db.product.create({
-      data: {
-        name,
-        company,
-        price,
-        image: "/images/product-1.jpg",
-        description,
-        featured,
-        clerkId: user.id,
-      },
-    });
+    const validatedFields = productSchema.parse(rawData);
 
     return { message: "product created" };
   } catch (error) {
