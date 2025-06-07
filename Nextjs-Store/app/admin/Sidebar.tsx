f2d5d02859eb7a8a46fcf0083e5f97ebbb655a0b
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-06-07 14:56:29 +0100

    Fix Sidebar component by adding a key prop to the Button for improved rendering

--- Nextjs-Store/app/admin/Sidebar.tsx
+++ Nextjs-Store/app/admin/Sidebar.tsx
@@ -16,7 +16,8 @@ function Sidebar() {
           <Button
             asChild
             className="w-full mb-2 capitalize font-normal justify-start"
-            variant={variant}>
+            variant={variant}
+            key={link.label}>
             <Link key={link.href} href={link.href}>
               {link.label}
             </Link>
